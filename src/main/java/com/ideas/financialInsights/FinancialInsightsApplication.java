c68217887b457ec34e73fa019c8ed5783e0c6647
Ricky Buvan <rickybuvan0219@outlook.com>
2022-08-10 18:24:42 +0530

    Initial Skeleton Code

---
+++ src/main/java/com/ideas/financialInsights/FinancialInsightsApplication.java
@@ -0,0 +1,13 @@
+package com.ideas.financialInsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialInsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialInsightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ideas/financialInsights/FinancialInsightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideas.financialInsights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialInsightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
